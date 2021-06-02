daaf5d2d58d86d1d80f3567919714cec1c2d3f1a
Ole994 <ole.korvald1994@gmail.com>
2021-06-03 00:53:28 +0200

    start

---
+++ js/hamburger.js
@@ -0,0 +1,22 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const body = document.querySelector ("body")
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    body.classList.toggle("body-on-click");
+}
+
+
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
